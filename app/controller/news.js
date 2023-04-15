c266be93b95cf87b2ebf00fcfb611d5ed7a7d8c4
DESKTOP-V1BF6TM\One smile <1539027251@qq.com>
2023-04-15 17:47:49 +0800

    init

---
+++ app/controller/news.js
@@ -0,0 +1,19 @@
+const { Controller } = require('egg');
+
+class NewsController extends Controller {
+  async list() {
+    /* const dataList = {
+      list: [
+        { id: 1, title: 'this is news 1', url: '/news/1' },
+        { id: 2, title: 'this is news 2', url: '/news/2' },
+      ],
+    }; */
+    const { ctx } = this;
+    const { page = 1 } = ctx.query;
+    const newsList = await ctx.service.news.list(page);
+
+    await this.ctx.render('news/list.tpl', { list: newsList });
+  }
+}
+
+module.exports = NewsController;
